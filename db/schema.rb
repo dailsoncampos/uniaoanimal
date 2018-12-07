a28bf429451ef69c58162057125a4e7eaa26e94e
Dailson Campos <dailson_campos@hotmail.com>
2018-12-07 10:20:24 -0300

    criacao da tabela Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_131013) do
+ActiveRecord::Schema.define(version: 2018_12_07_131907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street"
+    t.string "sector"
+    t.bigint "agent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["agent_id"], name: "index_addresses_on_agent_id"
+  end
 
   create_table "agents", force: :cascade do |t|
     t.string "name"
@@ -37,5 +46,6 @@ ActiveRecord::Schema.define(version: 2018_12_07_131013) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "agents"
   add_foreign_key "agents", "kinds"
 end
