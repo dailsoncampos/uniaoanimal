347b3cfd00f3511bca689ced522d669d1b45feac
Dailson Campos <dailson_campos@hotmail.com>
2018-12-07 10:10:41 -0300

    criacao da tabela Agent

---
+++ app/views/agents/_agent.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! agent, :id, :name, :phone, :email, :kind_id, :created_at, :updated_at
+json.url agent_url(agent, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_130628) do
+ActiveRecord::Schema.define(version: 2018_12_07_131013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "agents", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "email"
+    t.bigint "kind_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["kind_id"], name: "index_agents_on_kind_id"
+  end
 
   create_table "kind_humen", force: :cascade do |t|
     t.string "description"
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2018_12_07_130628) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "agents", "kinds"
 end
