8b1a7034050115e4a3611ea28611ee322b04e3d7
Dailson Campos <oidailsoncampos@gmail.com>
2018-12-09 13:55:39 -0300

    ajuste na configuracao do admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,9 +4,9 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-    warden.authenticate! scope: :user
+    warden.authenticate! scope: :agent
   end
-  config.current_user_method(&:current_user)
+  config.current_user_method(&:current_agent)
 
   ## == Cancan ==
   # config.authorize_with :cancan
