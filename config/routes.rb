468e1b14bbf3d48b06455e7ff6874c1da706749d
Dailson Campos <oidailsoncampos@gmail.com>
2018-12-09 11:59:27 -0300

    criacao de dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :agents
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   resources :events
   resources :addresses
   resources :agents
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
