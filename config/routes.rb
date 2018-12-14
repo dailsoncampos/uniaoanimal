9d5981bde5a65429625654b0dcb15125503133ac
Dailson Campos <dailson_campos@hotmail.com>
2018-12-14 17:44:21 -0300

    deletado scaffold Address

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   devise_for :agents
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   resources :events
-  resources :addresses
   resources :agents
   resources :tutors
   resources :animals
