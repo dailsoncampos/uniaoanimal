e51b92cea4c8f17d8d9bd0e2cb90fa53db977867
Dailson Campos <dailson_campos@hotmail.com>
2018-12-14 17:40:49 -0300

    adicionado campo de bairros

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -3,4 +3,5 @@ class Agent < ApplicationRecord
          :recoverable, :rememberable, :validatable
   enum category: [:tecnico, :coordenador, :voluntario, :usuario]
   enum status: [:ativo, :inativo]
+  enum sector: [:aeroporto_velho, :aldeia, :alvorada, :aparecida, :amparo, :area_verde, :caranazal, :cambuquira, :centro, :cohab, :conquista, :diamantino, :elcione_barbalho, :esperanca, :fatima, :floresta, :interventoria, :ipanema, :jaderlandia, :jardim_santarem, :jutai, :livramento, :liberdade, :laguinho, :maica, :Mapiri, :maracana, :maracana_1, :matinha, :nova_vitoria, :nova_republica, :novo_horizonte, :perola_do_maica, :prainha, :rodagem, :sale, :santana, :santa_clara, :santarenzinho, :santissimo, :santo_andre, :sao_cristovao, :sao_francisco, :sao_jose_operario, :uruara, :urumari, :vigia, :vitoria_regia]
 end
