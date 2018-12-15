526b3f6f69b1283cf14d707473943ac2d75afffc
Dailson Campos <oidailsoncampos@gmail.com>
2018-12-14 23:28:02 -0300

    retirada  a migracao de algumas tabelas

--- db/migrate/20181214141203_add_sector_as_reference_to_agent.rb
+++
@@ -1,5 +0,0 @@
-class AddSectorAsReferenceToAgent < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :agents, :sector, foreign_key: true
-  end
-end
--- db/migrate/20181214190829_remove_sector_from_agent.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSectorFromAgent < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :agents, :sector, foreign_key: true
-  end
-end
