4858202eedfbb847ec6147c2e7ff2f0979299f6f
Dailson Campos <dailson_campos@hotmail.com>
2018-12-21 18:14:11 -0300

    Pagina publica dos animais disponiveis para adocao

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :donors
   get '/eventos', to: "events_page#index"
+  get '/animais', to: "animals_page#index"
+  get '/animais/:condition', to: "animals_page#show", as: :search_show
   # get '/eventos/:id', to: "events_page#show", as: :evento
   devise_for :agents
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
