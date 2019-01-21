a77887d41bfeac7b4dc2ec5a2b350d0e1488fc66
Dailson Campos <dailson_campos@hotmail.com>
2019-01-21 17:50:32 -0300

    adicionais

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :donors
   get '/eventos', to: "events_page#index"
   get '/animais', to: "animals_page#index"
-  get '/animais/:condition', to: "animals_page#show", as: :search_show
-  # get '/eventos/:id', to: "events_page#show", as: :evento
+  get '/animais/:id', to: "animals_page#show", as: :search_show
   devise_for :agents
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   resources :events
