477ef52a7afc16f43018983ec2a9c88786fa0cc8
Dailson Campos <oidailsoncampos@gmail.com>
2019-01-23 23:00:03 -0300

    mudado a aparencia do filtro

--- app/controllers/animals_page_controller.rb
+++ app/controllers/animals_page_controller.rb
@@ -2,7 +2,6 @@ class AnimalsPageController < ApplicationController
   def index
     @animals_found = Animal.paginate(page: params[:page], per_page: 12).order(created_at: :desc)
     @q = Animal.ransack(params[:q])
-    # @animais = @q.result(distinct: true)
     @animais_search = @q.result.paginate(page: params[:page], per_page: 12).order(updated_at: :desc)
   end
 
