24501a5e53002dee4c608a3972b4a024cd672694
Dailson Campos <dailson_campos@hotmail.com>
2019-01-25 10:08:45 -0300

    definido layout para escolha do tipo de doacao

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,13 +11,23 @@ $(document).ready(function () {
     switch (this.value) {
       case "financeira":
         $(".description").hide();
-        $("#resposta").html("<h1>Doação Financeira</h1>");
+        $("#resposta").html("<h1 class=\"text-center\">ONG UNIÃO ANIMAL</h1><br><h2 class=\"text-center\">BANCO DO BRASIL</h2><br><h3 class=\"text-center\">Agência: 0130-9</h3><br><h3 class=\"text-center\">Conta Corrente: 95.715-1</h3><br><h3 class=\"text-center\">CNPJ: 23.567.091/0001-30</h3>");
         break;
       case "medicamento":
-        $("#resposta").html("<h1>Doação de Medicamentos</h1>");
+        $(".description").show();
+        $("#resposta").html("<h1>Shellder kyogre honchkrow illumise slugma excadrill klinklang clefable graveler girafarig</h1>");
+        break;
+      case "alimento":
+        $(".description").show();
+        $("#resposta").html("<h1>Skorupi nidoking luxio seadra dusknoir abra scrafty sunflora beedrill armaldo</h1>");
+        break;
+      case "outro":
+        $(".description").show();
+        $("#resposta").html("<h1>Shiftry doduo torterra ambipom azelf zigzagoon krookodile spinarak floatzel drapion</h1>");
         break;
       default:
-        $("#resposta").html("<h1>Escolha sua doação ao lado</h1>");
+        $(".description").show();
+        $("#resposta").html("<h1>Growlithe sneasel typhlosion gabite groudon lunatone snover swellow togepi vanilluxe</h1>");
     }
   });
 });
