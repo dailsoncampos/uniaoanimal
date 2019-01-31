9af475f8caf1dcf746c10ea2be56dfdc388731db
Dailson Campos <oidailsoncampos@gmail.com>
2019-01-31 05:16:30 -0300

    autenticacao de usuario

--- app/controllers/requesters_controller.rb
+++ app/controllers/requesters_controller.rb
@@ -1,5 +1,6 @@
 class RequestersController < ApplicationController
   before_action :set_requester, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /requesters
   # GET /requesters.json
