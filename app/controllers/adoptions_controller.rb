0f43517f1efa55e71386286c727f3636d31c1b11
Dailson Campos <oidailsoncampos@gmail.com>
2019-02-04 13:20:21 -0300

    solicitação de adoção

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,5 +1,6 @@
 class AdoptionsController < ApplicationController
   before_action :set_adoption, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_adopter!
 
   def index
     @adoptions = Adoption.all
@@ -10,18 +11,20 @@ class AdoptionsController < ApplicationController
 
   def new
     @adoption = Adoption.new
+    @animal = Animal.find(params[:animal_id])
+    @adopter = current_adopter
   end
 
   def edit
   end
 
   def create
-    @adoption = Adoption.new(adoption_params.except(:animal_id))
+    @adoption = Adoption.new(adoption_params)
 
     respond_to do |format|
       if @adoption.save
-        adopt_hash = adopter_params.to_h
-        Adoption.create(adopter_id: @adopter.id, animal_id: adopt_hash[:animal_id].to_i)
+        adopt_hash = adoption_params.to_h
+        Adoption.create(adopter_id: current_adopter.id.to_i, animal_id: adopt_hash[:animal_id].to_i)
         format.html { redirect_to @adoption, notice: 'Solicitação de adoção realizada com sucesso!' }
         format.json { render :show, status: :created, location: @adoption }
       else
--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,4 +1,11 @@
 class Adoption < ApplicationRecord
   belongs_to :adopter
   belongs_to :animal
+  enum status: [:pendente, :concluido]
+
+  before_create :set_adoption_status
+
+  def set_adoption_status
+    self.status ||= 0
+  end
 end
