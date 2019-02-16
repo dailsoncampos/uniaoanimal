3f762f751fb1a399efd2bae4f7efb92eefd15ea9
Dailson Campos <oidailsoncampos@gmail.com>
2019-02-16 11:20:02 -0300

    configuração assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Uniaoanimal
     config.load_defaults 5.2
     config.i18n.default_locale = :"pt-BR"
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
